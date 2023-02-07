98aed3588e5941d6baa36b8611306ba453b36480
jasperket <jasperkeith1.vallecera@gmail.com>
2023-02-07 16:43:24 +0800

    Finished reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(st) {
+    let newSt = '';
+    for(let i = st.length-1; i >= 0; i--) {
+        newSt = newSt + st.charAt(i);
+    }
+    return newSt;
 };
 
 // Do not edit below this line
