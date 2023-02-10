cf37cad94122bdb0dc641fa9ba54d501da7e3e1c
jasperket <jasperkeith1.vallecera@gmail.com>
2023-02-10 18:54:06 +0800

    Finished removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...items) {
+    for(const item of items) {
+        let i = arr.indexOf(item);
+        if(i != -1) {
+            arr.splice(i,1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
