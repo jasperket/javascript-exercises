5fa5dce9b881d07a5a0a564f2bd23c6a7ffdfb6f
jasperket <jasperkeith1.vallecera@gmail.com>
2023-02-15 19:47:23 +0800

    Finished 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const letters = str.replaceAll(/[^a-z]+/ig, '');
+    const len = letters.length;
+    let firstHalf, secondHalf;
+    if(len % 2 === 0) {
+        firstHalf = letters.slice(0, len/2);
+        secondHalf = letters.slice(len/2);
+    } else {
+        firstHalf = letters.slice(0, len/2);
+        secondHalf = letters.slice(len/2 +1);
+    }
+    for(let i = firstHalf.length-1, j = 0; i >= 0; i--, j++) {
+        if(firstHalf.charAt(i).toLowerCase() !== secondHalf.charAt(j).toLowerCase()) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
