458e140b514edb8e410125940af5f35ef09c36ce
jasperket <jasperkeith1.vallecera@gmail.com>
2023-02-16 19:15:35 +0800

    Finished 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+    if(nth < 0) {
+        return "OOPS";
+    }
+    let p = 0;
+    let p1 = 1;
+    let p2 = 1;
+    for(let i = 3; i <= nth; i++) {
+        p = p1 + p2;
+        p2 = p1;
+        p1 = p;
+    }
+    return p1;
 };
 
 // Do not edit below this line
